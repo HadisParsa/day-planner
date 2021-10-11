6b0e03d5f34954fd158dbaa5aaff2528a65c4652
Hadis Parsa <parsa_pary1988@yahoo.com>
2021-10-11 16:27:46 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# <Day-Planner>
+
+# Day Planner
+
+## Summery
+This project is about a simple calender that allows user to save events for typical workday (9am-5pm). The day planner blockes are colore coded to let the usetr know if it's gray meanse passed, red block shows the present time and future time blocks are green. 
+
+## Built with
+
+* Implement the Bootstrap grid layout in HTML.
+
+* Integrate Bootstrap components in HTML files to quickly build user interfaces.
+
+* Add custom styling to a CSS framework.
+
+* Implement jQuery UI to make a webpage more user-friendly.
+
+* Use [Moment.js ](https://momentjs.com/) to format dates and calculate time differences.
+
+ * [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+ * [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+ * [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+ * [jQuery](https://api.jquery.com/)
+ * [Bootstrap](https://getbootstrap.com/)
+ 
+
+
+## Link
+ [Click here to acces to the Work Day Scheduler]().
+
+# Table of Contents
+
+## Some screenshots of instructions are provided in following: 
+
+Here is what the day planner looks like: 
+    
+![alt text](assets/images/image1.jpg)
+
+![alt text](assets/images/image2.jpg)
+
+
+
+
---
+++ script.js
@@ -0,0 +1,47 @@
+$(init);
+
+function init() {
+  //On top of the page shows the present day
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  //blocks seting update every minute
+  TimeBlocks();
+  setInterval(TimeBlocks, 60000);
+
+  // Update time blocks with data in local storage
+  $(".block").each(function () {
+    var blockId = $(this).attr("id");
+    // load saved data from local storage
+    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+  // Save buttons attachment
+  $(".saveBtn").on("click", handleSave);
+}
+
+function TimeBlocks() {
+  // Time block set up
+  $(".block").each(function () {
+    var blockTiming = parseInt($(this).attr("id").replace("time-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    // remove a privious class 
+    $(this).removeClass("past present future");
+    // past, present and future bloks color set up
+    if (blockTiming < currentHour) {
+      $(this).addClass("past");
+    } else if (blockTiming > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+function handleSave(event) {
+  //Present id
+  var hourId = $(this).parent().attr("id");
+  // Save data in textarea
+  localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
+}
+
+
